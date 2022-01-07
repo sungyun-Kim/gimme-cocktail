e6f4f169c9451054ce98dc2e39472a977084246e
Niforances <niforances@gmail.com>
2022-01-07 11:34:25 +0900

    [Update] cocktail_card.dart
    lazy loading image

--- lib/widget/cocktail_card.dart
+++ lib/widget/cocktail_card.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:gimme_cocktail/vo/cocktail.dart';
 
 class CocktailCard extends StatelessWidget {
+  final Cocktail cocktail;
+
+  const CocktailCard({Key? key, required this.cocktail}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Center(
+      child: Card(
+        child: Column(
+          children: [
+            Text(cocktail.name),
+            Text(cocktail.isAlcoholic),
+            Image.network(cocktail.drinkThumb,
+                loadingBuilder: (context, child, loadingProgress) {
+                  if(loadingProgress == null) return child;
+                  return CircularProgressIndicator();
+                })
+          ],
+        ),
+      ),
     );
   }
 }
